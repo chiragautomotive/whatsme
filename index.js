31ad5e20d2ade92135a25398cfe2033409ad2c4c
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2022-06-23 17:46:55 +0530

    Add files via upload
    v1

---
+++ index.js
@@ -0,0 +1,7 @@
+function getPhoneNumber(){
+            // Selecting the input element and get its value 
+            var inputVal = 91+document.getElementById("phoneNumber").value;
+        var walink ="https://wa.me/"+inputVal
+ 
+  window.open("https://wa.me/"+inputVal+"?text=Dear%20Sir%2F%20Ma'am%2C%0AGreetings%20from%20Chirag%20Automotive%0AAddress%3A%20%0AChirag%20Automotive%20-%20Electric%20Vehicles%0AShop%2023%2F24%20Earth%20Eon%2COpp%20Urmi%20School%2C%20Sama-Savli%20Rd%2C%20New%20Sama%2C%20Vadodara%2C%20Gujarat%20390002%20%0Ahttps%3A%2F%2Fg.co%2Fkgs%2FvsdMqH")
+}
