1104d58f153e32a630e6a98c7c039e82ca5ba863
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2023-05-16 12:59:46 +0530

    Update index.js
    message update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getPhoneNumber(){
             var inputVal = 91+document.getElementById("phoneNumber").value;
         var walink ="https://wa.me/"+inputVal
  
-  window.open("https://wa.me/"+inputVal+"?text=Dear%20Sir%2F%20Ma'am%2C%0AGreetings%20from%20Chirag%20Automotive%0AAddress%3A%20%0AChirag%20Automotive%20-%20Electric%20Vehicles%0AShop%2023%2F24%20Earth%20Eon%2COpp%20Urmi%20School%2C%20Sama-Savli%20Rd%2C%20New%20Sama%2C%20Vadodara%2C%20Gujarat%20390002%20%0Ahttps%3A%2F%2Fg.co%2Fkgs%2FvsdMqH")
+  window.open("https://wa.me/"+inputVal+"?text=*Dear%20Patron,*%0A%0AGreetings%20from%20*Chirag%20Automotive*%20!!%0A%0AThank%20you%20for%20your%20interest%20in%20*Our%20wide%20range%20of%20Electric%20Vehicles*.%0A%0AAddress:%20%0A%0A*Chirag%20Automotive%20-%20Ampere%20Electric%20Vehicles*%0A%0AShop%2023/24%20Earth%20Eon,Opp%20Urmi%20School,%20Sama-Savli%20Rd,%20New%20Sama,%20Vadodara,%20Gujarat%20390008%0A%20%0Ahttps://g.co/kgs/vsdMqH%0A%0AFeel%20free%20to%20contact%20us%20for%20any%20further%20queries.%0A*7041756888*")
 }
